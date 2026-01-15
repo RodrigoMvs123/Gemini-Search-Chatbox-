95e415575561b069acc0d765ca6d7562ff083312
Rodrigo M.V.S. <88489374+RodrigoMvs123@users.noreply.github.com>
2026-01-15 11:59:12 +0000

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set the base path for deployment to GitHub Pages.
-  // Replace 'Gemini-Search-Chatbox-' with your actual repository name.
-  base: '/Gemini-Search-Chatbox-/',
+  base: '/',
 })
